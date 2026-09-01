e147b8dc50baa7208dc3466092c70583f6d8597a
mahan-dev-art <mahanpadvpor@google.com>
2026-09-01 23:24:08 +0330

    products page for admin panel added

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,6 +1,13 @@
 from flask import *
+from models.product import Product
+from config import db
 
 app = Blueprint("admin" , __name__)
+
+@app.before_request
+def before_request():
+    if session.get('admin_login' , None) == None and request.endpoint != 'admin.login':
+        abort(403)
 
 @app.route("/admin/login" , methods=["POST","GET"])
 def login():
@@ -18,6 +25,27 @@ def login():
     
 @app.route("/admin/dashboard" , methods=["GET"])
 def dashboard():
-    if session.get('admin_login' , None) == None :
-        abort(403)
     return render_template("admin/dashboard.html")
+    
+@app.route("/admin/dashboard/products" , methods=["GET" , "POST"])
+def products():
+    if request.method == "GET":
+        products = Product.query.all()
+        return render_template("admin/products.html" , products=products)
+    else:
+        name = request.form.get("name" , None)
+        description = request.form.get("description" , None)
+        price = request.form.get("price" , None)
+        active = request.form.get("active" , None)
+        
+        p = Product(name=name , description=description , price = price)
+        if active == None:
+            p.active = 0
+        else:
+            p.active = 1
+            
+        db.session.add(p)
+        db.session.commit()
+        
+        return "Done"
+    
--- models/product.py
+++ models/product.py
@@ -1,9 +1,10 @@
 from sqlalchemy import *
 from config import db
 
-class User(db.Model):
+class Product(db.Model):
     __tablename__ =  "products"
     id = Column(Integer , primary_key=True , index=True)
     name = Column(String , unique=True , nullable=False , index=True)
     description = Column(String, nullable=False , index=True)
     price = Column(Integer , nullable=False , index=True)
+    active = Column(Integer , nullable=False , index=True)
