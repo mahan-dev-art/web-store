1494294d211b6a5b82b7ce2ba4efbe57555e7745
mahan-dev-art <mahanpadvpor@google.com>
2026-09-01 22:43:46 +0330

    admin login and dashboard

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -2,6 +2,22 @@ from flask import *
 
 app = Blueprint("admin" , __name__)
 
-@app.route("/admin")
-def admin():
-    return "hello"
+@app.route("/admin/login" , methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username' , None)
+        password = request.form.get('password' , None)
+        
+        if username == "admin" and password == "1234":
+            session['admin_login'] = username
+            return redirect("/admin/dashboard")
+        else:
+            return redirect("/admin/login")
+    else:
+        return render_template("admin/login.html")
+    
+@app.route("/admin/dashboard" , methods=["GET"])
+def dashboard():
+    if session.get('admin_login' , None) == None :
+        abort(403)
+    return render_template("admin/dashboard.html")
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
+SECRET_KEY = "gfdgjsdbjcbsdugcsdbcfalavcdb654354"
 SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
 db = SQLAlchemy()
