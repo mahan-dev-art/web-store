5a3224beb740911cd1d3facd4f51cfe5b6a35668
mahan-dev-art <mahanpadvpor@google.com>
2026-09-02 15:29:23 +0330

    product page added

--- blueprints/general.py
+++ blueprints/general.py
@@ -5,8 +5,13 @@ app = Blueprint("general" , __name__)
 
 @app.route("/")
 def main():
-    products = Product.query.all()
+    products = Product.query.filter(Product.active == 1).all()
     return render_template("index.html" , products = products)
+
+@app.route("/product/<int:id>/<name>")
+def product(id , name):
+    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(Product.active == 1).first_or_404()
+    return render_template("product.html" , product = product)
 
 @app.route("/about")
 def about():
